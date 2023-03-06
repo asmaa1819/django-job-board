7a4d3c5bdf51a0dfb5cf9380ee36540a1224fc17
asmaa1819 <asmaaali932020@gmailcom>
2023-03-06 15:46:30 +0200

    add basic 4 apps in our project

--- job/models.py
+++ job/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+Job_type=[
+    ('full time','full time'),
+    ('part time','part time')
+]
 
 # Create your models here.
+class job(models.Model):
+    title=models.CharField(max_length=50)
+    job_type=models.CharField(max_length=15,choices=Job_type)
+    descrition =models.TextField(max_length=1000,default='descrition')
+    published_at =models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=1)
+    experiance=models.IntegerField(default=1)
+    catagory=models.ForeignKey('Category',on_delete=models.CASCADE)
+
+
+
+
+    def __str__(self):
+        return self.title
+
+class Category (models.Model):
+    title=models.CharField(max_length=50)  
+
+
+    def __str__(self):
+        return self.title      
+   
+
--- job/views.py
+++ job/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import job
 
 # Create your views here.
+def job_list (request):
+    job_list=job.objects.all()
+    context={'jobs':job_list}
+    return render(request,'job/job_list.html',context)
+
+def job_detal (request,id):
+    job_detal=job.objects.get(id=id)
+    context={'job':job_detal}
+    return render(request,'job/job_detal.html',context)
